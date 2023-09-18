b381f3b72cd94676eaece93df46bdcdbfba7d10d
W-T-M-O <wtmo_dev@naver.com>
2023-09-18 18:23:25 +0900

    feat: create def of input & output
    created skeleton of input & output of pig dice game

--- pig_dice.py
+++ pig_dice.py
@@ -2,6 +2,28 @@ class Pig_dice():
     def __init__(self):
         pass
 
+    def play_game(self, player1, player2):
+        pass
+
+    def roll_dice(self):
+        pass
+
+    def play_turn(player_name):
+        pass
+
+    def stay_or_stop(self):
+        count = 0
+        while count < 5:
+            option = input('please insert the option[stay,roll] : ')
+            if option == 'stay':
+                pass
+                break
+            elif option == 'roll':
+                pass
+                break
+            else:
+                print('Wrong intput')
+                count +=1
 
 if __name__ == "__main__":
     pig_dice = Pig_dice()
